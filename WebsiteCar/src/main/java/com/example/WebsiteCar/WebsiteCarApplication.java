092e67fd5fe305f70e5d360ab7b6d806fce36e4f
Tuan Anh <nguyentuananh.marc@gmail.com>
2021-06-24 17:34:21 +0700

    Start project

---
+++ WebsiteCar/src/main/java/com/example/WebsiteCar/WebsiteCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteCarApplication.class, args);
+	}
+
+}
